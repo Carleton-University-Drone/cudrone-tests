44373b06b6d061e073c4f15e9a0b5b1309dfaf75
smartinstot <davidb996@gmail.com>
2016-05-29 19:15:45 -0400

    Changed delay function to use timer0

--- main.c
+++ main.c
@@ -8,20 +8,26 @@
 
 #include <xc.h>
 #include "configBits.h"
+void delay(char c);
 void main(void) {
-    int loop;
     TRISB = 0;
     LATB = 0;
     while (1) {
-        loop = 0;
         LATB++;
-        while (loop < 30000) {
-            loop++;
-        }
+        delay(100);
     }
     return;
 }
 
-void delay(int c){
-    for(;c>0;c--);
+void delay(char c){
+    //enable timer
+    T0CON = 0b11001011;
+    //Clear current count
+    TMR0L=0;
+    
+    //Constantly poll timer to see if it goes surpasses threshold
+    while(TMR0L<c);
+    
+    //disable timer
+    T0CON = 0b01000000;
 }
