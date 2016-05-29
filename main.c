8462999389562b07eda299f27761f2ec2a57dd49
Thompson <Thompson@DESKTOP-B5DQODE>
2016-05-29 19:18:00 -0400

    Added second LED

--- main.c
+++ main.c
@@ -5,18 +5,30 @@
  * Created on May 29, 2016, 4:42 PM
  */
 
-
 #include <xc.h>
 #include "configBits.h"
+void delay(int c);
 void main(void) {
+    //Light Registers
     TRISB = 0;
     LATB = 0;
+    //Button Registers
+    PORTC = 0;
+    TRISC = 0x04;
     while (1) {
-        LATB = 0x10;
+        if (PORTC & 0x04 == 0x04) {
+            LATB = 0x10;
+        } else {
+            LATB = 0x20;
+        }
         delay(30000);
         LATB = 0;
         delay(10000);
-        LATB = 0x10;
+        if (PORTC & 0x04 == 0x04) {
+            LATB = 0x10;
+        } else {
+            LATB = 0x20;
+        }
         delay(10000);
         LATB = 0;
         delay(30000);
